9c39d41c25a69e927b41d33e5710b044b66d704c
Viktoria Korotkaya <viki.korotkaya@gmail.com>
2016-09-09 22:27:54 +0300

    Refactor 5

--- GetCurrencyRates.js
+++ GetCurrencyRates.js
@@ -21,120 +21,117 @@ var GetCurrencyRates = (function() {
       var yyyy = toDate.getFullYear();
       toDate = yyyy + '-' + mm + '-' + dd;
       return toDate;
+    },
+
+
+    generateChart: function() {
+      document.getElementById("progress").style.display = 'block';
+
+      var startDate = new Date(document.getElementById("start").value);
+      var endDate = new Date(document.getElementById("finish").value);
+
+      //функция создания массива дат для функции построения графика в формате YYYY-MM-DD
+      function createDateArray(startDate, endDate) {
+        var rightnow = startDate;
+        var result = [];
+        while (rightnow <= endDate) {
+          var dd = rightnow.getDate();
+          if (dd < 10) dd = '0' + dd;
+          var mm = rightnow.getMonth() + 1;
+          if (mm < 10) mm = '0' + mm;
+          var yyyy = rightnow.getFullYear();
+          var currentDate = yyyy + '-' + mm + '-' + dd;
+          result.push(currentDate);
+          rightnow.setDate(rightnow.getDate() + 1);
+        }
+        return result;
+      };
+      //создаем массив, который впоследствии будет содержать даты от и до
+      var arrDate = createDateArray(startDate, endDate);
+      //ajax-запрос, на выходе массив с данными по курсу валюты на выбранные даты arrCourse
+      function collectArrCourse(arrDate) {
+        var endpoint = 'historical';
+        var access_key = '38beb183813c14963a4b0813cd4f6640';
+        var currencies = 'BYR';
+        var arrCourse = [];
+        var i = 0;
+
+        function getCourse(i) {
+          $.ajax({
+            url: 'http://apilayer.net/api/' + endpoint + '?access_key=' + access_key + '&date=' + arrDate[i] + '&currencies=' + currencies,
+            dataType: 'jsonp',
+            success: function(json) {
+              arrCourse.push(json.quotes.USDBYR);
+              i++;
+              if (i < arrDate.length) {
+                getCourse(i);
+              } else {
+
+                var arrColor = getColorArray(arrCourse);
+                Highcharts.theme = {
+                  colors: arrColor,
+                  title: {
+                    style: {
+                      color: '#000',
+                      font: 'bold 20px "Trebuchet MS", Verdana, sans-serif'
+                    }
+                  },
+                  chart: {
+                    type: 'column'
+                  },
+                  plotOptions: {
+                    series: {
+                      colorByPoint: true
+                    }
+                  },
+                  yAxis: {
+                    min: null,
+                    max: null
+                  }
+                };
+                Highcharts.setOptions(Highcharts.theme);
+
+                var chart = $('#container').highcharts({
+
+
+                  title: {
+                    text: 'The change rate of the BYR against the dollar over time'
+                  },
+
+                  xAxis: {
+                    categories: arrDate
+                  },
+
+                  series: [{
+                    data: arrCourse,
+
+                  }]
+
+                });
+                document.getElementById("progress").style.display = 'none';
+
+              }
+            }
+          })
+        }
+        getCourse(i);
+      };
+
+      collectArrCourse(arrDate);
+
+      function getColorArray(arrCourse) {
+        var result = ['#7cb5ec'];
+        for (var i = 1; i < arrCourse.length; i++) {
+          if (arrCourse[i] / arrCourse[i - 1] >= 1.002) {
+            result.push('red');
+          } else if (arrCourse[i] / arrCourse[i - 1] <= 0.998) {
+            result.push('green');
+          } else {
+            result.push('#7cb5ec');
+          }
+        };
+        return result;
+      }
     }
   }
-}());
-
-function generateChart() {
-  document.getElementById("progress").style.display = 'block';
-  setTimeout(function() {
-    var startDate = new Date(document.getElementById("start").value);
-    var endDate = new Date(document.getElementById("finish").value);
-
-    //функция создания массива дат для функции построения графика в формате YYYY-MM-DD
-    function createDateArray(startDate, endDate) {
-      var rightnow = startDate;
-      var result = [];
-      while (rightnow <= endDate) {
-        var dd = rightnow.getDate();
-        if (dd < 10) dd = '0' + dd;
-        var mm = rightnow.getMonth() + 1;
-        if (mm < 10) mm = '0' + mm;
-        var yyyy = rightnow.getFullYear();
-        var currentDate = yyyy + '-' + mm + '-' + dd;
-        result.push(currentDate);
-        rightnow.setDate(rightnow.getDate() + 1);
-      }
-      return result;
-    };
-    //создаем массив, который впоследствии будет содержать даты от и до
-    var arrDate = createDateArray(startDate, endDate);
-    //ajax-запрос, на выходе массив с данными по курсу валюты на выбранные даты arrCourse
-    function collectArrCourse(arrDate) {
-      var endpoint = 'historical';
-      var access_key = '38beb183813c14963a4b0813cd4f6640';
-      var currencies = 'BYR';
-      var result = [];
-
-      function getCourse(i) {
-        $.ajax({
-
-          url: 'http://apilayer.net/api/' + endpoint + '?access_key=' + access_key + '&date=' + arrDate[i] + '&currencies=' + currencies,
-          dataType: 'json',
-          async: false,
-          success: function(json) {
-            result.push(json.quotes.USDBYR);
-          }
-        })
-      };
-
-      for (var i = 0; i < arrDate.length; i++) {
-        getCourse(i);
-      }
-      return result;
-    };
-
-
-    var arrCourse = collectArrCourse(arrDate);
-
-    function getColorArray(arrCourse) {
-      var result = ['#7cb5ec'];
-      for (var i = 1; i < arrCourse.length; i++) {
-        if (arrCourse[i] / arrCourse[i - 1] >= 1.002) {
-          result.push('red');
-        } else if (arrCourse[i] / arrCourse[i - 1] <= 0.998) {
-          result.push('green');
-        } else {
-          result.push('#7cb5ec');
-        }
-      };
-      return result;
-    };
-
-    var arrColor = getColorArray(arrCourse);
-
-    Highcharts.theme = {
-      colors: arrColor,
-      title: {
-        style: {
-          color: '#000',
-          font: 'bold 20px "Trebuchet MS", Verdana, sans-serif'
-        }
-      },
-      chart: {
-        type: 'column'
-      },
-      plotOptions: {
-        series: {
-          colorByPoint: true
-        }
-      },
-      yAxis: {
-        min: null,
-        max: null
-      }
-    };
-    Highcharts.setOptions(Highcharts.theme);
-
-    var chart = $('#container').highcharts({
-
-
-      title: {
-        text: 'The change rate of the BYR against the dollar over time'
-      },
-
-      xAxis: {
-        categories: arrDate
-      },
-
-      series: [{
-        data: arrCourse,
-
-      }]
-
-    });
-
-
-    document.getElementById("progress").style.display = 'none';
-  }, 5000);
-};
+})();
--- Main.js
+++ Main.js
@@ -1,8 +1,7 @@
 "use strict";
 $(document).ready(function() {
-  // Возможно, вы меня неправильно поняли, но разве в этом месте мы присваиваем Финальную дату и начальную дату, которую нельзя никак поменять??
-  // Подсказка: я имею ввиду, что непонятно, что происходит, если смотреть на названия методов
+  
   document.getElementById("finish").value = GetCurrencyRates.defaultFinalDate();
   document.getElementById("start").value = GetCurrencyRates.defaultInitialDate();
-  document.getElementById("button").onclick = generateChart;
+  document.getElementById("button").onclick = GetCurrencyRates.generateChart;
 });
